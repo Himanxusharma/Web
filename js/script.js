9e4395d13de0096c4128b8bbb867216d02fc7e10
Himanshu Sharma <himanshugaurbsr@gmail.com>
2023-04-03 21:00:43 +0530

    update further

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// document.getElementById("msg").setAttribute("class","xyz")
+// document.getElementById("msg").setAttribute("style","border:10px solid black;")
+// document.getElementById("msg").innerHTML = "<h1>Nothing</h1>"
+// document.querySelector("#msg").innerHTML="<h1>Pata nhi</h1>";
+
+document.querySelector("#msg").className="xyz";
