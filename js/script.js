6710447be7593acc1d49c8f9da0523c06015ec04
Himanshu Sharma <himanshugaurbsr@gmail.com>
2023-04-04 11:32:13 +0530

    update version

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
+var element;
 // document.getElementById("msg").setAttribute("class","xyz")
 // document.getElementById("msg").setAttribute("style","border:10px solid black;")
 // document.getElementById("msg").innerHTML = "<h1>Nothing</h1>"
 // document.querySelector("#msg").innerHTML="<h1>Pata nhi</h1>";
-
-document.querySelector("#msg").className="xyz";
+// document.querySelector("#msg").className="xyz";
+// document.querySelector("#msg").className="classList";
+// document.querySelector("#msg").style.backgroundColor = "tan";
+// element = document.querySelector("#msg").style.color = "white";
+// element = document.querySelector("#msg").style.border;
+// document.querySelector("#msg").className = "abc xyz";
+// document.querySelector("#msg").classList.add("def");
+// document.querySelector("#msg").classList.remove("def");
+// element = document.querySelector("#msg").classList;
+// console.log(element);
